0dc322c50eb65d39ad55dccbe9facf8446f51a99
Jamshid Qahramonov <qahramonovjamshid1@gmail.com>
2025-06-14 17:13:48 +0500

    done

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -20,6 +20,6 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('GoodBye World!');
+      .expect('Hello World!');
   });
 });
